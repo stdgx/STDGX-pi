cfb572f016f9ea8cb51a38f3b1d7868ae28df69a
andrefer-santtos <1andre.santos.souza1@gmail.com>
2023-10-20 08:51:07 -0300

    Creating Spring boot project and integrating dependencies for development

---
+++ stdgx/src/main/java/br/com/fatec/stdgx/StdgxApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.stdgx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdgxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdgxApplication.class, args);
+	}
+
+}
---
+++ stdgx/src/test/java/br/com/fatec/stdgx/StdgxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.stdgx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdgxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
