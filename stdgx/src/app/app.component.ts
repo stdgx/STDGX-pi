034dfb544c984753bf8729b1ba63c7003e6884f8
andrefer-santtos <1andre.santos.souza1@gmail.com>
2023-10-22 22:51:09 -0300

    Sidebar implemented!

--- stdgx/src/app/app.component.ts
+++ stdgx/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stdgx';
+
+  ngOnInit(): void {
+    this.sidebar();
+  }
+
+  sidebar() {
+    let sidebar = document.querySelector('.sidebar') as HTMLDivElement;
+    let sidebarBtn = document.querySelector('.bi-list-nested') as HTMLElement;
+
+    sidebarBtn.addEventListener('click', () => {
+      sidebar.classList.toggle('close');
+    });
+  }
 }
